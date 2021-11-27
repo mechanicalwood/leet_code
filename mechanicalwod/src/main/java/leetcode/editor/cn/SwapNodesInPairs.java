cd632aa019219707324fa8ab4dccf89e45831bc0
Jerry <17795837526@163.com>
2021-11-27 10:29:39 +0800

    2021-11-27-01

---
+++ mechanicalwod/src/main/java/leetcode/editor/cn/SwapNodesInPairs.java
@@ -0,0 +1,99 @@
+package leetcode.editor.cn;
+
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 100] 内 
+// 0 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。） 
+// Related Topics 递归 链表 
+// 👍 1133 👎 0
+
+public class SwapNodesInPairs{
+    public static void main(String[] args) {
+        Solution solution = new SwapNodesInPairs().new Solution();
+        
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (null != temp.next && null != temp.next.next) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
